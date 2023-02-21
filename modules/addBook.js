8f8b63b1443755a94b345bb0d9039d17c1a9e5a2
Muhammed Berisso <115731485+muhammedb8826@users.noreply.github.com>
2023-02-21 09:57:37 +0300

    Fixed linters error

--- modules/addBook.js
+++ modules/addBook.js
@@ -1,7 +1,6 @@
 const bookTitle = document.querySelector('#bookTitle');
 const bookAuthor = document.querySelector('#bookAuthor');
 const addBook = (bookCollection) => {
-  console.log(bookCollection);
   const book = { title: bookTitle.value, author: bookAuthor.value };
   bookCollection.push(book);
   bookAuthor.value = '';
